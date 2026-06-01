f3fa6246fa676555bca6187c1ff2047eeb58c8e4
Aditya Sinha <163927517+AdityaSinha06@users.noreply.github.com>
2026-06-02 04:05:51 +0530

    App.jsx

---
+++ GetRandomJokes/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Joker from './Joker.jsx'
+
+function App() {
+  return (
+    <>
+      <Joker></Joker>
+    </>
+  );
+}
+
+export default App
+
+// 4 pillars of react: components , props , state , event
+// state degign pattern : lifting state up
